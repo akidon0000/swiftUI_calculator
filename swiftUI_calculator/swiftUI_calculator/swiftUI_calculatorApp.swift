b190143c1a6d0425e3b97dba18fb209d2710d523
akidon0000 <a@art.jp>
2023-05-30 22:08:43 +0900

    [Add] Xcode project

---
+++ swiftUI_calculator/swiftUI_calculator/swiftUI_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_calculatorApp.swift
+//  swiftUI_calculator
+//
+//  Created by Akihiro Matsuyama on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
