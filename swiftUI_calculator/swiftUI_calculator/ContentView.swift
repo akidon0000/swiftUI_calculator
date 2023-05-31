23f76a46632cbee158ed907944c0a6b18a59a0bf
akidon0000 <a@art.jp>
2023-05-31 19:45:19 +0900

    [Fix] code cleanup

--- swiftUI_calculator/swiftUI_calculator/ContentView.swift
+++ swiftUI_calculator/swiftUI_calculator/ContentView.swift
@@ -8,12 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var resultScreen = "" // Textの内容を保存する変数
-    // Buttonで使うTextを配列にまとめる
-    let inputItem = [["9","8","7","÷"],
-                     ["6","5","4","×"],
-                     ["3","2","1","-"],
-                     ["0","C","=","+"]]
+    @State var resultScreen = "test" // Textの内容を保存する変数
     
     // グリッドレイアウトの定義
     let columns = [
@@ -24,10 +19,13 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        // コレクションビューの作成
-        ScrollView {
+        VStack{
+            Spacer()
+            Text(resultScreen) // ボタン入力のモニター
+                .font(.largeTitle)
+            Spacer()
             LazyVGrid(columns: columns, spacing: 30) {
-                ForEach((1...100), id: \.self) { index in
+                ForEach((1...20), id: \.self) { index in
                     // それぞれの要素はTextとします
                     Text("Item \(index)")
                         .frame(width: 60, height: 60)
@@ -37,97 +35,6 @@ struct ContentView: View {
             .padding()
         }
     }
-    
-
-//    var body: some View {
-//        VStack {
-//            Spacer()
-//            Text(resultScreen) // ボタン入力のモニター
-//                .font(.largeTitle)
-//            Spacer()
-//            ForEach((0...3), id: \.self) { row in
-//                HStack{
-//                    ForEach((0...3), id: \.self){ col in
-//                        Button(action: {
-//                            if((Int(self.inputItem[row][col])) != nil){
-//                                // 数値
-//                                self.resultScreen += self.inputItem[row][col]
-//                            }else{
-//                                if((row != 3) || (col == 3)){
-//                                    // 四則演算子
-//                                    let strArray = self.resultScreen.components(separatedBy: " ")
-//                                    if(strArray.count == 3){
-//                                        // 計算結果を表示する。
-//                                        let total = self.calculation(self.resultScreen)
-//                                        self.resultScreen = total
-//                                    }
-//                                    let paddingOperator = " " + self.inputItem[row][col] + " "
-//                                    self.resultScreen += paddingOperator
-//                                }else if(col == 1){
-//                                    // 入力内容を初期化
-//                                    self.resultScreen = ""
-//                                }else{
-//                                    // 計算結果を表示する。
-//                                    let total = self.calculation(self.resultScreen)
-//                                    self.resultScreen = total
-//                                }
-//                            }
-//                        }){
-//                            Spacer()
-//                            Text(self.inputItem[row][col])
-//                            Spacer()
-//                        }
-//                    }
-//                }
-//            .padding(30)
-//            }
-//        }
-//    }
-//    func calculation(_ concentsInputted: String) -> String{
-//        var nestInt = 0
-//        var total = 0
-//        var checkOperator = 0
-//        var checkNumber = true
-//        let strArray = concentsInputted.components(separatedBy: " ")
-//        for target in strArray {
-//            if(Int(target) != nil){
-//                if(checkNumber){
-//                    nestInt = Int(target)!
-//                    checkNumber.toggle()
-//                }else{
-//                    switch checkOperator {
-//                    case 1:
-//                        total += nestInt + Int(target)!
-//                        break
-//                    case 2:
-//                        total += nestInt - Int(target)!
-//                        break
-//                    case 3:
-//                        total += nestInt * Int(target)!
-//                        break
-//                    case 4:
-//                        total += nestInt / Int(target)!
-//                        break
-//                    default:
-//                        break
-//                    }
-//                    checkOperator = 0
-//                    checkNumber.toggle()
-//                }
-//            }else{
-//                if(target == "+"){
-//                    checkOperator = 1
-//                }else if(target == "-"){
-//                    checkOperator = 2
-//                }else if(target == "×"){
-//                    checkOperator = 3
-//                }else if(target == "÷"){
-//                    checkOperator = 4
-//                }
-//            }
-//        }
-//        return String(total)
-//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
